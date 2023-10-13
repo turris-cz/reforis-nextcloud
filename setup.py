beed48256114281a30d7f6f40a90a35f03cb141e
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2023-10-13 18:19:40 +0200

    setup: Update classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2019-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+#  Copyright (C) 2019-2023 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -66,11 +66,11 @@ setuptools.setup(
     },
     classifiers=[
         'Framework :: Flask',
-        'Intended Audience :: Developers',
-        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Development Status :: 5 - Production/Stable',
         'License :: Other/Proprietary License',
         'Natural Language :: English',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ],
