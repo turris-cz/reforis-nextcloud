a3a5ac7e20ce244a8d96fa984dc2ae9931f7892b
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-03-12 14:38:56 +0100

    Format Python files with Ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
-#  Copyright (C) 2019-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+#  Copyright (C) 2019-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
 
-pytest_plugins = 'reforis.test_utils.fixtures'
+pytest_plugins = "reforis.test_utils.fixtures"
